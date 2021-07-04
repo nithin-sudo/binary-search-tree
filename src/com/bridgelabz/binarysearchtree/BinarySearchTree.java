76a7a1647c273a27f3312254ca92f574498d7b15
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-04 14:48:37 +0530

    Added: initial setup for binary search tree program.

---
+++ src/com/bridgelabz/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.binarysearchtree;
+public class BinarySearchTree {
+
+}
---
+++ src/com/bridgelabz/binarysearchtree/Runner.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.binarysearchtree;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("welcome to binary search tree");
+    }
+}
+
